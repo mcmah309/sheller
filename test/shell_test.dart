8ca3603716923d942c011fdac4179738b0d207e7
mcmah309 <dillonmcmahon79@gmail.com>
2023-11-12 17:43:09 -0500

    test: Add tests

--- test/shell_test.dart
+++ test/shell_test.dart
@@ -1,8 +1,27 @@
-import 'package:shell/shell.dart';
+import 'package:sheller/src/shell.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Shell lazily throws error', () async {
+    final shell = Shell("exit 1");
+    final result = await shell.rawResult;
+    await Future.delayed(Duration(seconds: 1));
+    expect(() async => await shell.stringResult, throwsA(isA<ShellException>()));
+  });
+
+  test('int', () async {
+    final int y = await shell("echo 1");
+    expect(y, 1);
+  });
+
+  test('json', () async {
+    String data = '{"id":1, "name":"lorem ipsum", "address":"dolor set amet"}';
+    final Map<String, dynamic> w = await shell('echo $data');
+    expect(w.entries.length, 3);
+  });
+
+  test('List<double>', () async {
+    final List<double> d = await shell('echo 1 2 3');
+    expect(d.length, 3);
   });
 }
