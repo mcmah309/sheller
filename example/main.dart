5f30f5a40eb5a34b895284a772742436d658546a
mcmah309 <dillonmcmahon79@gmail.com>
2023-11-12 21:46:22 -0500

    refactor: Rename

--- example/main.dart
+++ example/main.dart
@@ -4,14 +4,14 @@ void main() async {
   // int
   int number = await shell("echo 1");
   assert(number == 1);
-// json
+  // json
   String data = '{"id":1, "name":"lorem ipsum", "address":"dolor set amet"}';
   Map<String, dynamic> json = await shell('echo $data');
   assert(json.entries.length == 3);
-// List<double>
+  // List<double>
   List<double> doubleList = await shell('echo 1 2 3');
   assert(doubleList.length == 3);
-// Class version
+  // Class version
   ShellSync shellClass = ShellSync("echo 1");
   int id = shellClass.rawResult.pid; // shell.rawResult.runtimeType == ProcessResult
   String stringResult = shellClass.stringResult; // == "1"
