27becdd28942a62ee4fb6a46d7221f3c9e00e2ba
mcmah309 <dillonmcmahon79@gmail.com>
2024-05-18 17:16:59 +0000

    test: testing impl in ci

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,11 +1,13 @@
 import 'dart:io' as io;
 
+import 'shell_base.dart';
+
 (String, List<String>) createPlatformExecutableAndArgs(String cmd){
     final String executable;
     final List<String> args;
     if(io.Platform.isWindows){
       executable = "C:\\WINDOWS\\system32\\cmd.exe";
-      args = ["/c", cmd];
+      args = ["/c", ...cmd.split($.whitespacesExp)];
     }
     else if(io.Platform.isLinux || io.Platform.isMacOS){
       executable = "/bin/sh";
